4e0ceaaa1b9c79b1fe29c81958fa263db5868927
Jesús García <js.garcia.fernandez@gmail.com>
2022-12-15 22:12:20 +0100

    last hot fixes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,6 @@
 <template>
   <footer :class="theme">
-    <div :class="themeLogo">
-      <!-- <img
-        src="https://res.cloudinary.com/dz3wb5q87/image/upload/v1670971750/Final-yisus-project/Logo%20images/proyecto_logo-final-black_rgjagp.png"
-        alt="footer-logo"
-      /> -->
-    </div>
+    <div :class="themeLogo"></div>
     <div class="contact-us-container">
       <p>Contact Us</p>
       <p>Calle Falsa, 123</p>
